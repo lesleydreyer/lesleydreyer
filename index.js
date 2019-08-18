3f1cb6a6d0211dec7d487e9b4e812f6ca7ea885f
Lesley <lesleydreyer@hotmail.com>
2019-08-17 18:58:26 -0700

    worked on styling and contact section

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-    $('.js--section-features').waypoint(function (direction) {
-        if (direction == "down") {
-            $('nav').addClass('navbar-light bg-light ');
-        } else {
-            $('nav').removeClass('navbar-light bg-light ');
-        }
-    }, {
-            offset: '60px;'
+$(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
         });
+    }, false);
+})();
+
+$('.js--section-features').waypoint(function (direction) {
+    if (direction == "down") {
+        $('nav').addClass('navbar-light bg-light ');
+    } else {
+        $('nav').removeClass('navbar-light bg-light ');
+    }
+}, {
+        offset: '60px;'
+    });
 
         // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
 //var mainbottom = $('#main').offset().top + $('#main').height();
