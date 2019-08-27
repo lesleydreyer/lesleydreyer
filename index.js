f1b20beb32a83f3d23f10ab7fae63f238fb45ed4
Lesley <lesleydreyer@hotmail.com>
2019-08-26 17:44:54 -0700

    worked on navbar, contact submit button, and styling

--- index.js
+++ index.js
@@ -1,75 +1,53 @@
-jQuery(document).ready(function ($) {
+const $ = window.jQuery;
 
-    //alert('test');
 
-    // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
-    var homeBottom = $('#home').offset().top + $('#home').height();
-    console.log('home-bot' + homeBottom);
+//THIS IS FOR THE NAVBAR COLOR CHANGE ON SCROLL
+$(document).ready(function ($) {
+    // get the value of the bottom of the #home element by adding the offset of that element plus its height, set it as a variable
+    let homeBottom = $('#home').offset().top + $('#home').height();
 
-    // on scroll, 
     $(window).on('scroll', function () {
-        console.log('test');
-
         // we round here to reduce a little workload
         stop = Math.round($(window).scrollTop());
-        console.log('stop' + stop);
+        //if scrolls beyond home screen and medium width (not mobile) add the past-home class to make the navbar black text, 
+        //and if goes before home screen reverse back make navbar white text
         if (stop > homeBottom && $(document).width() >= 576) {
-            $('.nav').addClass('past-home');
-            $('nav').removeClass('navbar-mobile');
-        } else if (stop > homeBottom && $(document).width() < 576) {
-            $('.nav').removeClass('past-home');
-            $('nav').addClass('navbar-mobile');
+            $('.navbar').addClass('past-home');
+        } else if (stop < homeBottom && $(document).width() >= 576) {
+            $('.navbar').removeClass('past-home');
+            $('.navbar').addClass('before-home');
         }
-        else {
-            $('.nav').removeClass('past-home');
-        }
-
     });
-
 });
 
-$(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})(jQuery);
-
-// $('.js--section-features').waypoint(function (direction) {
-//     if (direction == "down") {
-//         $('nav').addClass('navbar-light bg-light ');
-//     } else {
-//         $('nav').removeClass('navbar-dark bg-dark ');
+//started to convert from vanilla JS to jquery 
+// let homeBottom = $('#home').offset().top + $('#home').height();
+// window.onscroll = function() {
+//     let stop = Math.round(document.body.scrollTop || document.documentElement.scrollTop);
+//     if (stop > homeBottom && $(document).width() >= 576) {
+//         var el = document.getElementsByClassName('navbar')
+//         el.classList.add('past-home');
+//     } else if (stop < homeBottom && $(document).width() >= 576) {
+//         $('.navbar').removeClass('past-home');
+//         $('.navbar').addClass('before-home');
 //     }
-// }, {
-//         offset: '60px;'
-//     });
+// }
 
-// get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
-//var mainbottom = $('#main').offset().top + $('#main').height();
 
-// on scroll, 
-// $(window).on('scroll',function(){
 
-//     // we round here to reduce a little workload  
-//     var stop = Math.round($(window).scrollTop());
-
-//     if (stop > mainbottom) {
-//         $('.nav').addClass('past-main');
-//     } else {
-//         $('.nav').removeClass('past-main');
-//     }
-
-// });
-
+//FORM VALIDITY - RECOMMENDED TO ADD ON BOOTSTRAP DOCUMENTATION
+'use strict';
+window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+}, false);
